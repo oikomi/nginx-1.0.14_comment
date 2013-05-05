e3b0f24552d0c9c42470276296b3632ed1a4fb87
Harold <miaohonghit@gmail.com>
2013-05-05 13:02:41 +0800

    init

--- src/http/modules/ngx_http_static_module.c
+++ src/http/modules/ngx_http_static_module.c
@@ -267,7 +267,7 @@ ngx_http_static_init(ngx_conf_t *cf)
 
     cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
 
-    h = ngx_array_push(&cmcf->phases[NGX_HTTP_CONTENT_PHASE].handlers);
+    h = ngx_array_push(&cmcf->phases[NGX_HTTP_CONTENT_PHASE].handlers);//挂载这个handler到NGX_HTTP_CONTENT_PHASE处理阶段
     if (h == NULL) {
         return NGX_ERROR;
     }
